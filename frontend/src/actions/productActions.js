644dbab94afd4451c217e410ddd5cec1580171cf
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-03-14 16:01:48 +0300

    end with prodcut details page

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,8 +4,12 @@ import {
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstants';
 
+// get All the products
 export const getProducts = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_PRODUCTS_REQUEST });
@@ -14,6 +18,19 @@ export const getProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+//get a product details
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data.product });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response.data.message,
     });
   }
