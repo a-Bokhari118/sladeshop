a2d99cd5861ebcc8e5513e98347499b484e6d6e9
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-03-16 13:45:47 +0300

    add profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,13 @@ import Header from './components/layout/Header';
 import ProductDetails from './components/product/ProductDetails';
 import Login from './components/user/Login';
 import Register from './components/user/Register';
+import Profile from './components/user/Profile';
+
+// load loggedin user
 import { loadUser } from './actions/userActions';
 import store from './store';
+// ================================
+
 function App() {
   useEffect(() => {
     store.dispatch(loadUser());
@@ -24,6 +29,7 @@ function App() {
           <Route path='/product/:id' component={ProductDetails} exact />
           <Route path='/login' component={Login} exact />
           <Route path='/register' component={Register} exact />
+          <Route path='/me' component={Profile} exact />
         </div>
         <Footer />
       </div>
